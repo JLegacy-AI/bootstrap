f286de8954e5c30c8b39529b289d4d9b4e474924
Khadija Saleem <khadijasaleem027@gmail.com>
2022-06-11 23:11:37 +0500

    Home completed

--- files/src/components/screens/home/HomePage.js
+++ files/src/components/screens/home/HomePage.js
@@ -1,8 +1,75 @@
 import React, { useState } from "react";
 import { Accordion, Button, Col, Container, Row } from "react-bootstrap";
 import "./home-page.css";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+import Img1 from "../../../assets/img/icon-card-1.png";
+import Img2 from "../../../assets/img/icon-card-2.png";
+import Img3 from "../../../assets/img/icon-card-3.png";
+import Img4 from "../../../assets/img/icon-card-4.png";
+import HomeMaraige from "./HomeMaraige";
+import HomeProfessionalEvent from "./HomeProfessionalEvent";
+import HomeAnniversary from "./HomeAnniversary";
+import HomeReligiousEvent from "./HomeReligiousEvent";
 
 const HomePage = () => {
+  const [eventType, setEventType] = useState({
+    type: 1,
+    color: "#0ca672",
+    background: "#24775b",
+    img: Img1,
+  });
+  const responsive = {
+    superLargeDesktop: {
+      breakpoint: { max: 4000, min: 3000 },
+      items: 4,
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 4,
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2,
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1,
+    },
+  };
+  const ButtonGroup = ({ next, previous, goToSlide, ...rest }) => {
+    const {
+      carouselState: { currentSlide, totalItems, slidesToShow },
+    } = rest;
+    const disableState = totalItems - slidesToShow;
+    return (
+      <div className="d-flex justify-content-between mt-3 mb-3">
+        <h2 className="clt-section-3-h2">Mariage</h2>
+        <div>
+          <Button
+            className={
+              currentSlide === 0
+                ? "disable clt-section-3-cards-arrowBtn me-2"
+                : "clt-section-3-cards-arrowBtn me-2"
+            }
+            onClick={() => previous()}
+          >
+            <i class="fa-solid fa-chevron-left"></i>
+          </Button>
+          <Button
+            className={
+              currentSlide === disableState
+                ? "disable clt-section-3-cards-arrowBtn"
+                : "clt-section-3-cards-arrowBtn"
+            }
+            onClick={() => next()}
+          >
+            <i class="fa-solid fa-chevron-right"></i>
+          </Button>
+        </div>
+      </div>
+    );
+  };
   return (
     <React.Fragment>
       <Container>
@@ -35,49 +102,161 @@ const HomePage = () => {
       <Container fluid>
         <Row className="clt-section-2-row">
           <Col>
-            <Container>
+            <Container className="clt-section-2-row-container">
               <Row>
-                <Col>
+                <Col className="clt-section-2-row-col">
                   <div className="d-flex justify-content-start align-items-center clt-section-2">
-                    <div className="d-flex flex-column justify-content-center align-items-center clt-section-2-divs">
+                    <div
+                      className="d-flex flex-column justify-content-center align-items-center clt-section-2-divs"
+                      onClick={() =>
+                        setEventType({
+                          type: 1,
+                          color: "#0ca672",
+                          background: "#24775b",
+                          img: Img1,
+                        })
+                      }
+                    >
                       <img
                         src={
-                          require(`../../../assets/img/icon-bar-1.png`).default
+                          require(`../../../assets/img/${
+                            eventType.type === 1
+                              ? "icon-bar-1-active"
+                              : "icon-bar-1"
+                          }.png`).default
                         }
                         className=""
                         alt="ParkingAeroPortFr"
                       />
-                      <h2>Mariage</h2>
+                      <h2
+                        style={
+                          eventType.type === 1
+                            ? {
+                                color: `${eventType.color}`,
+                                borderBottom: `2px solid ${eventType.color}`,
+                              }
+                            : {
+                                color: `#7f7f7f`,
+                                borderBottom: `2px solid transparent`,
+                              }
+                        }
+                      >
+                        Mariage
+                      </h2>
                     </div>
-                    <div className="d-flex flex-column justify-content-center align-items-center clt-section-2-divs">
+                    <div
+                      className="d-flex flex-column justify-content-center align-items-center clt-section-2-divs"
+                      onClick={() =>
+                        setEventType({
+                          type: 2,
+                          color: "#015eea",
+                          background: "#015eea",
+                          img: Img2,
+                        })
+                      }
+                    >
                       <img
                         src={
-                          require(`../../../assets/img/icon-bar-2.png`).default
+                          require(`../../../assets/img/${
+                            eventType.type === 2
+                              ? "icon-bar-2-active"
+                              : "icon-bar-2"
+                          }.png`).default
                         }
                         className=""
                         alt="ParkingAeroPortFr"
                       />
-                      <h2>Événement professionnel</h2>
+                      <h2
+                        style={
+                          eventType.type === 2
+                            ? {
+                                color: `${eventType.color}`,
+                                borderBottom: `2px solid ${eventType.color}`,
+                              }
+                            : {
+                                color: `#7f7f7f`,
+                                borderBottom: `2px solid transparent`,
+                              }
+                        }
+                      >
+                        Événement professionnel
+                      </h2>
                     </div>
-                    <div className="d-flex flex-column justify-content-center align-items-center clt-section-2-divs">
+                    <div
+                      className="d-flex flex-column justify-content-center align-items-center clt-section-2-divs"
+                      onClick={() =>
+                        setEventType({
+                          type: 3,
+                          color: "#ad1eac",
+                          background: "#c72fc3",
+                          img: Img3,
+                        })
+                      }
+                    >
                       <img
                         src={
-                          require(`../../../assets/img/icon-bar-3.png`).default
+                          require(`../../../assets/img/${
+                            eventType.type === 3
+                              ? "icon-bar-3-active"
+                              : "icon-bar-3"
+                          }.png`).default
                         }
                         className=""
                         alt="ParkingAeroPortFr"
                       />
-                      <h2>Anniversaire</h2>
+                      <h2
+                        style={
+                          eventType.type === 3
+                            ? {
+                                color: `${eventType.color}`,
+                                borderBottom: `2px solid ${eventType.color}`,
+                              }
+                            : {
+                                color: `#7f7f7f`,
+                                borderBottom: `2px solid transparent`,
+                              }
+                        }
+                      >
+                        Anniversaire
+                      </h2>
                     </div>
-                    <div className="d-flex flex-column justify-content-center align-items-center clt-section-2-divs">
+                    <div
+                      className="d-flex flex-column justify-content-center align-items-center clt-section-2-divs"
+                      onClick={() =>
+                        setEventType({
+                          type: 4,
+                          color: "#b3846d",
+                          background: "#d2997e",
+                          img: Img4,
+                        })
+                      }
+                    >
                       <img
                         src={
-                          require(`../../../assets/img/icon-bar-4.png`).default
+                          require(`../../../assets/img/${
+                            eventType.type === 4
+                              ? "icon-bar-4-active"
+                              : "icon-bar-4"
+                          }.png`).default
                         }
                         className=""
                         alt="ParkingAeroPortFr"
                       />
-                      <h2>Événement religieux</h2>
+                      <h2
+                        style={
+                          eventType.type === 4
+                            ? {
+                                color: `${eventType.color}`,
+                                borderBottom: `2px solid ${eventType.color}`,
+                              }
+                            : {
+                                color: `#7f7f7f`,
+                                borderBottom: `2px solid transparent`,
+                              }
+                        }
+                      >
+                        Événement religieux
+                      </h2>
                     </div>
                   </div>
                 </Col>
@@ -86,194 +265,10 @@ const HomePage = () => {
           </Col>
         </Row>
       </Container>
-      <Container>
-        <Row>
-          <Col>
-            <div className="clt-section-3">
-              <h2>Mariage</h2>
-              <div>
-                <div className="d-flex flex-row justify-content-start align-items-center clt-section-3-cards-div">
-                  <div className="d-flex justify-content-start align-items-start clt-section-3-cards clt-section-3-cards-main">
-                    <h2>Votre lieu de mariage</h2>
-                  </div>
-                  <div className="d-flex flex-column justify-content-start align-items-start clt-section-3-cards clt-section-3-cards-secondary">
-                    <h2>1</h2>
-                    <div>
-                      <span>Le château</span>
-                      <p>
-                        Ce lieu permet d'accueillir des cérémonies et des
-                        réceptions jusqu’à 100 invités.
-                      </p>
-                    </div>
-                  </div>
-                  <div className="d-flex flex-column justify-content-start align-items-start clt-section-3-cards clt-section-3-cards-secondary">
-                    <h2>2</h2>
-                    <div>
-                      <span>Optionnel: Prestataire</span>
-                      <p>Traiteur, fleur, décoration, photographe…</p>
-                      <p>
-                        Une sélection de prestataire pouvant vous accompagner
-                      </p>
-                    </div>
-                  </div>
-                  <div className="d-flex flex-column justify-content-start align-items-start clt-section-3-cards clt-section-3-cards-secondary">
-                    <h2>3</h2>
-                    <div>
-                      <span>Optionnel: Dortoir</span>
-                      <p>
-                        Faites dormir vous et vos proches directement dans le
-                        château.
-                      </p>
-                    </div>
-                  </div>
-                  <div className="d-flex flex-column justify-content-start align-items-start clt-section-3-cards clt-section-3-cards-end">
-                    <h2>
-                      Retrouver toutes les informations sur la page du château
-                    </h2>
-                    <Button
-                      onClick={() => console.log("clicked!")}
-                      className="clt-section-3-cards-button shadow-none"
-                    >
-                      <span>
-                        En savoir plus{" "}
-                        <i className="fa-solid fa-arrow-right"></i>
-                      </span>
-                    </Button>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </Col>
-        </Row>
-      </Container>
-      <Container>
-        <Row>
-          <Col>
-            <div className="border_bottom_light">
-              <div className="clt-section-4">
-                <img
-                  src={
-                    require(`../../../assets/img/home-section-4.png`).default
-                  }
-                  className=""
-                  alt="ParkingAeroPortFr"
-                />
-                <h2>Planifiez votre événement idéal</h2>
-              </div>
-            </div>
-          </Col>
-        </Row>
-      </Container>
-      <Container>
-        <Row>
-          <Col>
-            <div className="clt-section-5">
-              <h2 className="clt-section-5-h2">
-                Questions fréquentes pour votre événement
-              </h2>
-              <Accordion
-                defaultActiveKey="0"
-                className="clt-section-5-accordion"
-              >
-                <Accordion.Item
-                  eventKey="0"
-                  className="clt-section-5-accordion-item"
-                >
-                  <Accordion.Header className="clt-section-5-accordion-header">
-                    Ask 1 - Wedding
-                  </Accordion.Header>
-                  <Accordion.Body className="clt-section-5-accordion-body">
-                    <p>Answer 1 - Wedding</p>
-                    <a href="#">Tous les sujets</a>
-                  </Accordion.Body>
-                </Accordion.Item>
-                <Accordion.Item
-                  eventKey="1"
-                  className="clt-section-5-accordion-item"
-                >
-                  <Accordion.Header className="clt-section-5-accordion-header">
-                    Ask 2 - Wedding
-                  </Accordion.Header>
-                  <Accordion.Body className="clt-section-5-accordion-body">
-                    <p>Answer 2 - Wedding</p>
-                    <a href="#">Tous les sujets</a>
-                  </Accordion.Body>
-                </Accordion.Item>
-              </Accordion>
-            </div>
-          </Col>
-        </Row>
-      </Container>
-      <Container>
-        <Row>
-          <Col>
-            <div className="border_bottom_light">
-              <div className="clt-section-6">
-                <h2>Retrouvez plus de détail sur notre page dédiée</h2>
-                <img
-                  src={
-                    require(`../../../assets/img/home-section-6.png`).default
-                  }
-                  className=""
-                  alt="ParkingAeroPortFr"
-                />
-                <Button
-                  onClick={() => console.log("clicked!")}
-                  className="clt-section-6-btn shadow-none"
-                >
-                  <span>Continuer</span>
-                </Button>
-              </div>
-            </div>
-          </Col>
-        </Row>
-      </Container>
-      <Container>
-        <Row>
-          <Col>
-            <div className="border_bottom_light">
-              <div className="clt-section-7">
-                <h2>Besoin d’aide ?</h2>
-                <p>
-                  Résolvez votre problème ici. Nous vous mettrons en relation
-                  avec le support téléphonique si besoin.
-                </p>
-                <Button
-                  onClick={() => console.log("clicked!")}
-                  className="clt-section-7-btn shadow-none"
-                >
-                  <span>
-                    Visitez le centre d’aide{" "}
-                    <i className="fa-solid fa-arrow-right"></i>
-                  </span>
-                </Button>
-              </div>
-            </div>
-          </Col>
-        </Row>
-      </Container>
-      <Container>
-        <Row>
-          <Col>
-            <div className="">
-              <div className="clt-section-8">
-                <div className="d-flex flex-column mb-5">
-                  <a href="#">Centre d’aide</a>
-                  <a href="#">Connexion</a>
-                  <a href="#">Créer un compte</a>
-                </div>
-                <div>
-                  <p>@2022 Château la tournelle</p>
-                  <p>
-                    <a href="#">Conditions générales de ventes</a> -{" "}
-                    <a href="#">Mentions légales</a>
-                  </p>
-                </div>
-              </div>
-            </div>
-          </Col>
-        </Row>
-      </Container>
+      {eventType.type === 1 && <HomeMaraige eventType={eventType} />}
+      {eventType.type === 2 && <HomeProfessionalEvent eventType={eventType} />}
+      {eventType.type === 3 && <HomeAnniversary eventType={eventType} />}
+      {eventType.type === 4 && <HomeReligiousEvent eventType={eventType} />}
     </React.Fragment>
   );
 };
